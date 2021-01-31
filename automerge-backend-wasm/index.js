73df8d5752636e2db289b1b73844b99985d24a62
Irakli Gozalishvili <contact@gozala.io>
2021-01-30 22:06:39 -0800

    feat: turn into esm

--- automerge-backend-wasm/index.js
+++ automerge-backend-wasm/index.js
@@ -1,84 +1,146 @@
-let Backend = require('./pkg')
-const util = require('util');
+import { State } from "./pkg/index.js"
 
-function init() {
-  return { state: Backend.State.new(), heads: [], frozen: false }
+/**
+ * @typedef {Object} Backend
+ * @property {State} state
+ * @property {any[]} heads
+ * @property {boolean} frozen
+ */
+
+/**
+ * @returns {Backend}
+ */
+export function init() {
+  return { state: State.new(), heads: [], frozen: false }
 }
 
-function load(data) {
-  const state = Backend.State.load(data)
+/**
+ * @template Data
+ * @param {Data} data
+ * @returns {Backend}
+ */
+export function load(data) {
+  const state = State.load(data)
   const heads = state.getHeads()
   return { state, heads, frozen: false }
 }
 
+/**
+ * @param {Backend} backend
+ * @returns {State}
+ */
 function backendState(backend) {
   if (backend.frozen) {
     throw new Error(
-      'Attempting to use an outdated Automerge document that has already been updated. ' +
-      'Please use the latest document state, or call Automerge.clone() if you really ' +
-      'need to use this old document state.'
+      "Attempting to use an outdated Automerge document that has already been updated. " +
+        "Please use the latest document state, or call Automerge.clone() if you really " +
+        "need to use this old document state."
     )
   }
   return backend.state
 }
 
-function clone(backend) {
-  const state = backend.state.clone();
+/**
+ * @param {Backend} backend
+ * @returns {Backend}
+ */
+export function clone(backend) {
+  const state = backend.state.clone()
   return { state, heads: backend.heads.slice(), frozen: false }
 }
 
-function free(backend) {
+/**
+ * @param {Backend} backend
+ * @returns {void}
+ */
+export function free(backend) {
   backend.state.free()
   backend.state = null
   backend.frozen = true
 }
 
-function applyChanges(backend, changes) {
+/**
+ * @param {Backend} backend
+ * @param {any[]} changes
+ * @returns {[Backend, any]}
+ */
+export function applyChanges(backend, changes) {
   const state = backendState(backend)
-  const [patch,heads] = state.applyChanges(changes)
+  const [patch, heads] = state.applyChanges(changes)
   backend.frozen = true
   return [{ state, heads, frozen: false }, patch]
 }
 
-function applyLocalChange(backend, request) {
+/**
+ * @param {Backend} backend
+ * @param {any} request
+ * @returns {[Backend, any, any]}
+ */
+export function applyLocalChange(backend, request) {
   const state = backendState(backend)
-  const [patch,change, heads] = state.applyLocalChange(request)
+  const [patch, change, heads] = state.applyLocalChange(request)
   backend.frozen = true
   return [{ state, heads, frozen: false }, patch, change]
 }
 
-function loadChanges(backend, changes) {
+/**
+ * @param {Backend} backend
+ * @param {any[]} changes
+ * @returns {Backend}
+ */
+export function loadChanges(backend, changes) {
   const state = backendState(backend)
   const heads = state.loadChanges(changes)
   backend.frozen = true
   return { state, heads, frozen: false }
 }
 
-function getPatch(backend) {
+/**
+ * @param {Backend} backend
+ * @returns {any}
+ */
+export function getPatch(backend) {
   return backendState(backend).getPatch()
 }
 
-function getChanges(backend, clock) {
+/**
+ * @param {Backend} backend
+ * @param {any} clock
+ * @returns {any[]}
+ */
+export function getChanges(backend, clock) {
   return backendState(backend).getChanges(clock)
 }
 
-function getChangesForActor(backend, actor) {
+/**
+ * @param {Backend} backend
+ * @param {any} actor
+ * @returns {any[]}
+ */
+export function getChangesForActor(backend, actor) {
   return backendState(backend).getChangesForActor(actor)
 }
 
-function getMissingDeps(backend) {
+/**
+ * @param {Backend} backend
+ * @returns {any}
+ */
+export function getMissingDeps(backend) {
   return backendState(backend).getMissingDeps()
 }
 
-function save(backend) {
+/**
+ * @param {Backend} backend
+ * @returns {Uint8Array}
+ */
+export function save(backend) {
   return backendState(backend).save()
 }
 
-function getHeads(backend) {
+/**
+ * @param {Backend} backend
+ * @returns {any[]}
+ */
+export function getHeads(backend) {
   return backend.heads
-}
-
-module.exports = {
-  init, clone, save, load, free, applyChanges, applyLocalChange, loadChanges, getPatch,
-  getChanges, getChangesForActor, getMissingDeps, getHeads
 }
